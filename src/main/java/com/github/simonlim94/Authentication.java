49a886ae760ca2c0c6e2c35023c0e0b45aff72f0
simon <simon@revenuemonster.my>
2019-01-14 15:30:43 +0800

    add authorization code

--- src/main/java/com/github/simonlim94/Authentication.java
+++ src/main/java/com/github/simonlim94/Authentication.java
@@ -64,6 +64,64 @@ public class Authentication {
         return result;
     }
 
+    public ClientCredentials GetAuthorizationCode(final String Code, final String RedirectUri){
+        ClientCredentials result = new ClientCredentials();
+        String parameter = env.clientId+":"+env.clientSecret;
+        String encodeParameter = Encode.Base64Encode(parameter);
+        String targetUrl = "";
+
+        if(env.environment == "sandbox"){
+            targetUrl = Url.SandBoxOAuth + "/v1/token";
+        }else if(env.environment == "production"){
+            targetUrl = Url.ProductionOAuth + "/v1/token";
+        }
+
+        String basicAuth  ="Basic "+encodeParameter;
+        try {
+            Object values = new Object() {
+                String grantType = "authorization_code";
+                String code = Code;
+                String redirectUri = RedirectUri;
+            };
+            ObjectMapper myObjectMapper = new ObjectMapper();
+            myObjectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            ObjectWriter ow = myObjectMapper.writer().withDefaultPrettyPrinter();
+            String json = ow.writeValueAsString(values);
+
+            URL url = new URL(targetUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Authorization", basicAuth);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json;");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            // write json into body
+            OutputStream os = connection.getOutputStream();
+            os.write(json.getBytes("UTF-8"));
+            os.close();
+
+            // read the response
+            int code = connection.getResponseCode();
+            InputStream in = null;
+            if(code > 200 ) {
+                in = connection.getErrorStream();
+            }else {
+                in = new BufferedInputStream(connection.getInputStream());
+            }
+            String response = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+            System.out.println(response);
+            result = myObjectMapper.readValue(response,ClientCredentials.class);
+
+            in.close();
+            connection.disconnect();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+
     public ClientCredentials RefreshToken(){
         ClientCredentials result = new ClientCredentials();
         String parameter = env.clientId+":"+env.clientSecret;
